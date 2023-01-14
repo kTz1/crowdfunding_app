93ba268f7623352a1f78c7def496d9ffe315b51e
kTz1 <catalin_simon90@yahoo.com>
2023-01-14 14:17:40 +0200

    fetch data from user for profile page

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -48,6 +48,15 @@ export const StateContextProvider = ({ children }) => {
     return parsedCampaigns;
   }
 
+  // get user campaigns
+  const getUserCampaigns = async () => {
+    const allCampaigns = await getCampaigns();
+
+    const filteredCampaigns = allCampaigns.filter((campaign) => campaign.owner === address);
+
+    return filteredCampaigns;
+  }
+
   return (
     <StateContext.Provider 
       value={{ 
@@ -56,6 +65,7 @@ export const StateContextProvider = ({ children }) => {
         connect,
         createCampaign: publishCampaign,
         getCampaigns,
+        getUserCampaigns,
       }}
     >
       {children}
